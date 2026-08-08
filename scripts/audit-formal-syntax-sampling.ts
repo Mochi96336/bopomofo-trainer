368fa99c1fa67d84c804268b139c5ffc555c45af
Mochi96336 <144137376+Mochi96336@users.noreply.github.com>
2026-08-08 19:15:00 +0800

    chore: report product family priors in syntax audit

--- scripts/audit-formal-syntax-sampling.ts
+++ scripts/audit-formal-syntax-sampling.ts
@@ -1,6 +1,31 @@
-import { auditEqualRuleTicketDistribution } from "../src/curriculum/formal-syntax-taxonomy.js";
+import {
+  PRODUCT_FORMAL_SYNTAX_SAMPLING_POLICY,
+  sentenceConstructionFamilyPrior,
+} from "../src/curriculum/formal-syntax-sampling-policy.js";
+import {
+  auditEqualRuleTicketDistribution,
+  type SentenceConstructionFamily,
+} from "../src/curriculum/formal-syntax-taxonomy.js";
 import { FORMAL_SYNTAX_RULES } from "../src/syntax/grammar.js";
 
-const audit = auditEqualRuleTicketDistribution(FORMAL_SYNTAX_RULES);
+const sentenceFamilies: readonly SentenceConstructionFamily[] = [
+  "statement.declarative",
+  "statement.complex",
+  "question.polar",
+  "question.a-not-a",
+  "question.alternative",
+  "question.constituent",
+  "request",
+  "exclamative",
+];
 
-console.log(JSON.stringify(audit, null, 2));
+console.log(JSON.stringify({
+  equalRuleTickets: auditEqualRuleTicketDistribution(FORMAL_SYNTAX_RULES),
+  productPolicy: {
+    version: PRODUCT_FORMAL_SYNTAX_SAMPLING_POLICY.version,
+    sentenceFamilyPriors: Object.fromEntries(sentenceFamilies.map((family) => [
+      family,
+      sentenceConstructionFamilyPrior(family),
+    ])),
+  },
+}, null, 2));
