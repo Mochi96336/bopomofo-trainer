fd1234aa981d463640180dd5cbb759b5ae811ea0
Mochi96336 <144137376+Mochi96336@users.noreply.github.com>
2026-08-08 20:20:48 +0800

    refactor: derive audit families from sampling policy

--- scripts/audit-formal-syntax-sampling.ts
+++ scripts/audit-formal-syntax-sampling.ts
@@ -1,29 +1,16 @@
 import {
   PRODUCT_FORMAL_SYNTAX_SAMPLING_POLICY,
+  SENTENCE_CONSTRUCTION_FAMILIES,
   sentenceConstructionFamilyPrior,
 } from "../src/curriculum/formal-syntax-sampling-policy.js";
-import {
-  auditEqualRuleTicketDistribution,
-  type SentenceConstructionFamily,
-} from "../src/curriculum/formal-syntax-taxonomy.js";
+import { auditEqualRuleTicketDistribution } from "../src/curriculum/formal-syntax-taxonomy.js";
 import { FORMAL_SYNTAX_RULES } from "../src/syntax/grammar.js";
-
-const sentenceFamilies: readonly SentenceConstructionFamily[] = [
-  "statement.declarative",
-  "statement.complex",
-  "question.polar",
-  "question.a-not-a",
-  "question.alternative",
-  "question.constituent",
-  "request",
-  "exclamative",
-];
 
 console.log(JSON.stringify({
   equalRuleTickets: auditEqualRuleTicketDistribution(FORMAL_SYNTAX_RULES),
   productPolicy: {
     version: PRODUCT_FORMAL_SYNTAX_SAMPLING_POLICY.version,
-    sentenceFamilyPriors: Object.fromEntries(sentenceFamilies.map((family) => [
+    sentenceFamilyPriors: Object.fromEntries(SENTENCE_CONSTRUCTION_FAMILIES.map((family) => [
       family,
       sentenceConstructionFamilyPrior(family),
     ])),
